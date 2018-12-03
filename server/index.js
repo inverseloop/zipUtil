0db73cf9b3e1f4d9dff547f8fa1398dac25ec23a
whipjack <robderr@gmail.com>
2018-12-03 16:29:35 -0500

    Initial check in for the zipUtil REST API.

---
+++ server/index.js
@@ -0,0 +1,38 @@
+//index.js
+//
+
+'use strict';
+
+const koa = require('koa')
+const app = new koa()
+const PORT = process.env.PORT || 1337;
+//routes
+const zipRoutes = require('./routes/zip')
+
+// logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(zipRoutes.routes())
+
+app.use(async ctx => {
+  ctx.status = 418
+  ctx.body = "I'm a teapot"
+});
+
+const server = app.listen(PORT, () => {
+          console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = server;
---
+++ server/routes/zip.js
@@ -0,0 +1,35 @@
+const Router = require('koa-router');
+
+const router = new Router();
+const BASE_URL = `/api/v1/zip`;
+
+const zips = require('./zips')
+
+module.exports = router;
+
+router.get(BASE_URL + '/:id', async (ctx) => {
+	  console.log('ctx= '+ctx)
+  try {
+        console.log('zip with zip')
+        ctx.body = {
+        status: 'success',
+        data: zips[ctx.params.id]
+        }
+  } catch (err) {
+    console.log('Zip router err:' + err)
+  }
+})
+
+router.get(BASE_URL, async (ctx) => {
+	  console.log('ctx= '+ctx)
+  try {
+    console.log('zips!')
+    ctx.body = {
+      status: 'success',
+      data: zips
+    };
+  } catch (err) {
+    console.log('Zip router err:' + err)
+  }
+})
+
